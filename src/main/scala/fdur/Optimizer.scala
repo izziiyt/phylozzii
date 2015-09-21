dc883a05feb4a6a288f6ee0533939670a8f5f7c8
yuto <ahne.ahne.ahne@gmail.com>
2015-09-21 17:05:24 +0900

    will implement QEstep

--- src/main/scala/fdur/Optimizer.scala
+++ src/main/scala/fdur/Optimizer.scala
@@ -10,23 +10,27 @@ import scala.collection.parallel.mutable.ParArray
 object Optimizer extends LazyLogging {
   type Column = Array[Base]
 
-  def em(itemax: Int, nhf: String, maf: String, pi:DenseVector[Double], b:DenseVector[Double]): (List[Double], Parameters) = {
+  protected def emLike(itemax: Int, nhf: String, maf: String, pi:DenseVector[Double], b:DenseVector[Double],
+             estepFunc:(ModelRoot,List[Column],Model) => (VD,List[MD],List[VD],Double,Int)): (List[Double], Parameters) = {
     var tree = ModelTree.fromFile(nhf)
     val cols = Maf.readMaf(maf, 1000).toParArray
     var param = Parameters(b, pi)
     var i = 0
-    var check = true
-    var currentlgl:Double = Double.NegativeInfinity
-    while(i < itemax && check) {
+    var converged = false
+    var currentlgl = Double.NegativeInfinity
+    while(i < itemax && !converged) {
       val model = Model(param)
-      val suffs = cols.map(estep(tree, _, model))
+      val suffs = cols.map(estepFunc(tree, _, model))
       val (lgl, br, pr) = mstep(suffs, model, tree.branches)
+      converged =
+        util.doubleEqual(br,tree.branches,1.0E-8) &&
+        util.doubleEqual(pr.pi,param.pi,1.0E-8) &&
+        util.doubleEqual(pr.Bvec,param.Bvec,1.0E-8)
       tree = tree.changeBranches(br)
       param = pr
-      check = !util.doubleEqual(currentlgl,lgl)
       if(i > 1 && currentlgl > lgl) {
         logger.error("log likelihood decreased.")
-        check = false
+        converged = true
       }
       currentlgl = lgl
       i += 1
@@ -35,32 +39,16 @@ object Optimizer extends LazyLogging {
     regularize(tree.branches, param)
   }
 
-  def ldem(itemax: Int, nhf: String, maf: String, pi:DenseVector[Double], b:DenseVector[Double]): (List[Double], Parameters) = {
-    var tree = ModelTree.fromFile(nhf)
-    val cols = Maf.readMaf(maf, 1000).toParArray
-    var param = Parameters(b, pi)
-    var i = 0
-    var check = true
-    var currentlgl:Double = Double.NegativeInfinity
-    while(i < itemax && check) {
-      val model = Model(param)
-      val suffs = cols.map(ldestep(tree, _, model))
-      val (lgl, br, pr) = mstep(suffs, model, tree.branches)
-      tree = tree.changeBranches(br)
-      param = pr
-      check = !util.doubleEqual(currentlgl,lgl)
-      if(i > 1 && currentlgl > lgl) {
-        logger.error("log likelihood decreased.")
-        check = false
-      }
-      currentlgl = lgl
-      i += 1
-    }
-    logger.info("optimized_log_likelihood=" + currentlgl + " iteration=" + i)
-    regularize(tree.branches, param)
-  }
 
-  def gd(maxit:Int,nh:String,maf:String,pi:DenseVector[Double],b:DenseVector[Double]) = {
+
+  def em(itemax: Int, nhf: String, maf: String, pi:DenseVector[Double], b:DenseVector[Double]) =
+    emLike(itemax,nhf,maf,pi,b,estep)
+
+  def ldem(itemax: Int, nhf: String, maf: String, pi:DenseVector[Double], b:DenseVector[Double]) =
+    emLike(itemax,nhf,maf,pi,b,ldestep)
+
+  protected def gdLike(maxit:Int,nh:String,maf:String,pi:DenseVector[Double],b:DenseVector[Double],
+                        mapper:(ModelRoot,List[Array[Base]],Model) => (VD,MD,List[Double],Double)) = {
     val cols = Maf.readMaf(maf, 1000).toParArray
     val template = ModelTree.fromFile(nh)
 
@@ -70,7 +58,7 @@ object Optimizer extends LazyLogging {
         val branch = p(10 until p.length).toArray.toList
         val tree = template.changeBranches(branch)
         val model = Model(param)
-        val diffs = cols.map(gradMap(tree, _, model))
+        val diffs = cols.map(mapper(tree, _, model))
         val (regb, regpi) = mkreg(param)
         val (nlgl, newp) = gradReduce(diffs, regb, regpi)
         (nlgl,newp)
@@ -85,30 +73,10 @@ object Optimizer extends LazyLogging {
     regularize(brnch,param)
   }
 
-  def ldgd(maxit:Int,nh:String,maf:String,pi:DenseVector[Double],b:DenseVector[Double]) = {
-    val cols = Maf.readMaf(maf, 1000).toParArray
-    val template = ModelTree.fromFile(nh)
-
-    val f = new DiffFunction[VD] {
-      def calculate(p: VD) = {
-        val param = Parameters(p(0 to 9))
-        val branch = p(10 until p.length).toArray.toList
-        val tree = template.changeBranches(branch)
-        val model = Model(param)
-        val diffs = cols.map(ldgradMap(tree, _, model))
-        val (regb, regpi) = mkreg(param)
-        val (nlgl, newp) = gradReduce(diffs, regb, regpi)
-        (nlgl,newp)
-      }
-    }
-
-    val lbfgs = new LBFGS[VD](maxIter = maxit, m = 3)
-    val iniparam = DenseVector.vertcat(b,pi,DenseVector(template.branches.toArray))
-    val optparam = lbfgs.minimize(f, iniparam)
-    val param = Parameters(optparam(0 to 9))
-    val brnch = optparam(10 until optparam.length).toArray.toList
-    regularize(brnch,param)
-  }
+  def ldgd(maxit:Int,nh:String,maf:String,pi:DenseVector[Double],b:DenseVector[Double]) =
+    gdLike(maxit,nh,maf,pi,b,ldgradMap)
+  def gd(maxit:Int,nh:String,maf:String,pi:DenseVector[Double],b:DenseVector[Double]) =
+    gdLike(maxit,nh,maf,pi,b,gradMap)
 
   protected def mkreg(p: Parameters) = {
     val pitmp = DenseMatrix.vertcat(p.pi.asDenseMatrix,p.pi.asDenseMatrix,p.pi.asDenseMatrix,p.pi.asDenseMatrix)
@@ -119,11 +87,14 @@ object Optimizer extends LazyLogging {
     (b, pi)
   }
 
-  protected def mkLog(lgl: Double, tree: ModelRoot, param: Parameters):String =
-    "\n" + "loglikelihood=" + lgl + "\n" + tree.toString + "\n" + "pi=" + param.pi + "\n" + "b=" + param.Bvec
+  //protected def mkLog(lgl: Double, tree: ModelRoot, param: Parameters):String =
+  //  "\n" + "loglikelihood=" + lgl + "\n" + tree.toString + "\n" + "pi=" + param.pi + "\n" + "b=" + param.Bvec
 
-  def estep(pr:ModelRoot,cols:List[Column],model:Model): (VD,List[MD],List[VD],Double,Int) = Tree.suffStat(pr,model,cols)
-  def ldestep(pr:ModelRoot,cols:List[Column],model:Model): (VD,List[MD],List[VD],Double,Int) = LDTree.suffStat(pr,model,cols)
+  protected def estep(pr:ModelRoot,cols:List[Column],model:Model): (VD,List[MD],List[VD],Double,Int) =
+    Tree.suffStat(pr,model,cols)
+
+  protected def ldestep(pr:ModelRoot,cols:List[Column],model:Model): (VD,List[MD],List[VD],Double,Int) =
+    LDTree.suffStat(pr,model,cols)
 
   def mstep(suffs: ParArray[(VD, List[MD], List[VD], Double, Int)],model:Model, branches:List[Double]):
   (Double, List[Double], Parameters) = {
@@ -142,7 +113,7 @@ object Optimizer extends LazyLogging {
     (lgl, br, pr)
   }
 
-  def gradMap(pr:ModelRoot,cols:List[Array[Base]],model:Model): (VD,MD,List[Double],Double) = {
+  protected def gradMap(pr:ModelRoot,cols:List[Array[Base]],model:Model): (VD,MD,List[Double],Double) = {
     val tree = Tree.inout(pr,model,cols)
     val pi = tree.diffWithPi.reduceLeft(_ + _)
     val b = tree.diffWithB.reduceLeft(_ + _)
@@ -151,7 +122,7 @@ object Optimizer extends LazyLogging {
     (pi,b,br,likelihood)
   }
 
-  def ldgradMap(pr:ModelRoot,cols:List[Array[Base]],model:Model): (VD,MD,List[Double],Double) = {
+  protected def ldgradMap(pr:ModelRoot,cols:List[Array[Base]],model:Model): (VD,MD,List[Double],Double) = {
     val tree = LDTree.inout(pr,model,cols)
     val pi = tree.diffWithPi.reduceLeft(_ + _)
     val b = tree.diffWithB.reduceLeft(_ + _)
@@ -160,7 +131,7 @@ object Optimizer extends LazyLogging {
     (pi,b,br,likelihood)
   }
 
-  def gradReduce(grads:ParArray[(VD,MD,List[Double],Double)],breg:MD,pireg:MD)
+  protected def gradReduce(grads:ParArray[(VD,MD,List[Double],Double)],breg:MD,pireg:MD)
   : (Double,VD) = {
     val (pi, b, br, lgl): (VD, MD, List[Double], Double) = grads.reduce {
       (x, y) =>
@@ -177,7 +148,7 @@ object Optimizer extends LazyLogging {
     (-lgl, -DenseVector.vertcat(bgrad,pigrad,brgrad))
   }
 
-  def regularize(brnc:List[Double],param:Parameters): (List[Double],Parameters) = {
+  protected def regularize(brnc:List[Double],param:Parameters): (List[Double],Parameters) = {
     val model = Model(param)
     val summ = - (0 to 3).foldLeft(0.0){(x,i) => x + model.pi(i) * model.R(i,i)}
     val br = brnc map (_ * summ)
