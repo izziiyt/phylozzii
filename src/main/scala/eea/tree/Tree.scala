7f9378880c90b71066fa899b01f14a36599d8cdf
yuto <ahne.ahne.ahne@gmail.com>
2015-10-28 17:05:23 +0900

    change eea.tree.Tree

--- src/main/scala/eea/tree/Tree.scala
+++ src/main/scala/eea/tree/Tree.scala
@@ -1,5 +1,258 @@
-/*package eea.tree
+package eea.tree
 
+import fdur._
+import breeze.linalg._
+import alignment.Base
+import breeze.math.LogDouble
+import breeze.numerics.exp
+import scala.annotation.tailrec
+import breeze.math.LogDouble.SemiringLogDouble
+
+trait LDTree extends PrimitiveTree{
+  def alpha: Array[VL]
+  def alphaD: Array[VL]
+  def beta: Array[VL]
+  def betaD: Array[VL]
+  def trans: ML
+  def transD: ML
+  def model: Model
+  lazy val insideProp: Array[VL] = {
+    require(alpha.nonEmpty)
+    alpha.map(a => trans.t * a)
+  }
+  lazy val insidePropD: Array[VL] = {
+    require(alpha.nonEmpty)
+    alphaD.map(a => transD.t * a)
+  }
+
+  def toList: List[LDTree]
+}
+
+object LDTree extends LDTreeUtilTrait {
+  @tailrec
+  protected def innerIn(ch: List[ModelChild], cols: List[Array[Base]], result: List[LDChild], model:Model,target:String):
+  (List[LDChild], List[Array[Base]]) = {
+    if (ch == Nil)
+      (result.reverse, cols)
+    else {
+      val (newTree,newCols) = inside(ch.head,model,cols,target)
+      innerIn(ch.tail, newCols, newTree :: result, model,target)
+    }
+  }
+
+  protected def inside(tr: ModelChild, model: Model, columns: List[Array[Base]],target:String): (LDChild, List[Array[Base]]) =
+    tr match {
+      case ModelLeaf(name,t) =>
+        (LDLeaf.inside(name,t,columns.head,model,name == target),columns.tail)
+      case ModelNode(ch,t) =>
+        val tmp = innerIn(ch,columns,Nil,model,target)
+        (LDNode.inside(tmp._1,t,model),tmp._2)
+    }
+
+  def inside(tr: ModelRoot, model: Model, columns: List[Array[Base]], target:String): LDRoot = {
+    val (newCh,newCols) = innerIn(tr.children,columns,Nil,model,target)
+    if(newCols.nonEmpty) sys.error("Bad Columns")
+    LDRoot.inside(newCh,model)
+  }
+
+  @tailrec
+  protected def innerOut(prefix:List[LDChild],suffix:List[LDChild],result:List[LDChild], fromThis:Array[VL],fromThisD:Array[VL]):
+  List[LDChild] = {
+    if(prefix.isEmpty) result.reverse
+    else{
+      innerOut(prefix.tail, prefix.head :: suffix,
+        outside(prefix.head,(suffix ::: prefix.tail).map(_.insideProp),fromThis,
+          (suffix ::: prefix.tail).map(_.insidePropD),fromThisD) :: result,fromThis,fromThisD)
+    }
+  }
+
+  def mkBeta(fromSib:List[Array[VL]],fromPar:Array[VL]):Array[VL] = mkAlpha(fromPar :: fromSib)
+
+  def mkBetaD(fromSib:List[Array[VL]],fromPar:Array[VL],fromSibD:List[Array[VL]],fromParD:Array[VL]):Array[VL] =
+    mkAlphaD(fromPar :: fromSib, fromParD :: fromSibD)
+
+  protected def outside(tree:LDChild, fromSib:List[Array[VL]], fromPar:Array[VL],
+                        fromSibD:List[Array[VL]],fromParD:Array[VL]): LDChild = {
+    val beta = mkBeta(fromSib,fromPar)
+    val betaD = mkBetaD(fromSib,fromPar,fromSibD,fromParD)
+    lazy val fromThis = outsideProp(beta,tree.trans)
+    lazy val fromThisD = outsideProp(betaD,tree.transD)
+    tree match {
+      case LDLeaf(name, t, trans, transD, alpha, alphaD, _, _, model) =>
+        LDLeaf(name,t,trans,transD,alpha,alphaD,beta,betaD,model)
+      case LDNode(ch,t,trans,transD,alpha,alphaD,_,_,model) =>
+        val newch = innerOut(ch,Nil,Nil,fromThis,fromThisD)
+        LDNode(newch,t,trans,transD,alpha,alphaD,beta,betaD,model)
+    }
+  }
+
+  def outside(tr:LDRoot):LDRoot = {
+    require(tr.alpha.nonEmpty)
+    val n = tr.alpha.length
+    val beta = Array.fill(n)(tr.model.pi.toLogDouble)
+    val betaD = Array.fill(n)(DenseVector.zeros[Double](4).toLogDouble)
+    val fromThis = outsideProp(beta,tr.trans)
+    val fromThisD = outsideProp(betaD,tr.transD)
+    val newch = innerOut(tr.children,Nil,Nil,fromThis,fromThisD)
+    LDRoot(newch,tr.trans,tr.transD,tr.alpha,tr.alphaD,beta,betaD,tr.model)
+  }
+
+  protected def outsideProp(beta:Array[VL],trans:ML): Array[VL] = {
+    require(beta.nonEmpty)
+    beta.map(b => trans * b)
+  }
+
+  def inout(tr: ModelRoot, model: Model, columns: List[Array[Base]],target:String): LDRoot = {
+    require(columns.forall(_.length == columns.head.length))
+    require(tr.leafLength == columns.length)
+    val afterIn = inside(tr, model, columns, target)
+    outside(afterIn)
+  }
+
+  def bls(tr:ModelRoot,model:Model,columns:List[Array[Base]],target:String): Array[Double] = {
+    val root = inout(tr,model,columns,target)
+    root.bls
+  }
+
+}
+
+trait LDChild extends LDTree with PrimitiveChild{
+  def model:Model
+
+  def toList:List[LDTree]
+
+  def leafList:List[LDLeaf]
+
+  lazy val dltA: DenseMatrix[Double] = {
+    val tmp = for(from <- 0 to 3; to <- 0 to 3) yield
+    (0 to 3).foldLeft(0.0){(n,x) => n + model.u(to,x) * model.ui(x,from) * k(t * model.R(to,to), t * model.lambda(x))}
+    new DenseMatrix[Double](4,4,tmp.toArray) * t
+  }
+
+  lazy val dltB: DenseMatrix[Double] = {
+    val tmp = for(from <- 0 to 3; to <- 0 to 3) yield
+    (0 to 3).foldLeft(0.0){(n,x) => n + model.u(to,x) * model.ui(x,from) * k(t * model.lambda(x), t * model.R(from,from))}
+    new DenseMatrix[Double](4,4,tmp.toArray) * t
+  }
+
+  protected def k(x:Double,y:Double):Double = {
+    val tmp = (exp(x) - exp(y)) / (x - y); if(tmp.isNaN) exp(x) else tmp
+  }
+
+  protected def thisBls(fromPar:Array[VL],fromParD:Array[VL]): Array[Double] = {
+    def f(as:Array[VL],bs:Array[VL],dlt:MD):Array[Double] = (as,bs).zipped.map {
+      (a, b) =>
+        val tmp = diag(a.value) * DenseMatrix.ones[Double](4,4) * diag(b.value)
+        val reg = sum(tmp)
+        sum(tmp :* dlt) / (if(reg == 0.0) 1.0 else reg)
+    }
+    val ax = f(alphaD, beta, dltA)
+    val bx = f(alpha, betaD, dltB)
+    (ax, bx).zipped.map(_ + _)
+  }
+
+  def bls(fromPar:Array[VL], fromParD:Array[VL]): Array[Double]
+}
+
+trait LDParent extends LDTree with PrimitiveParent {
+  def leafList: List[LDLeaf] = children.foldLeft(Nil:List[LDLeaf])((n,x) => x.leafList ::: n)
+  override def children: List[LDChild]
+  def toList: List[LDTree] = this :: children.foldLeft(Nil:List[LDTree])((n,x) => x.toList ::: n)
+  def bls: Array[Double] = {
+    require(beta.nonEmpty && betaD.nonEmpty)
+    val n = beta.length
+    children.foldLeft(Array.fill[Double](n)(0.0))((m,x) => (m,x.bls(beta, betaD)).zipped.map(_ + _))
+  }
+}
+
+case class LDLeaf(name:String, t:Double, trans:ML, transD:ML, alpha:Array[VL], alphaD:Array[VL], beta:Array[VL],
+                betaD:Array[VL], model:Model) extends LDChild with PrimitiveLeaf{
+  def toList:List[LDTree] = this :: Nil
+  def leafList = this :: Nil
+  def bls(fromPar:Array[VL],fromParD:Array[VL]) = thisBls(fromPar,fromParD)
+}
+
+case class LDNode(children:List[LDChild], t:Double, trans:ML, transD:ML,alpha:Array[VL], alphaD:Array[VL],
+                beta:Array[VL], betaD:Array[VL], model:Model) extends LDChild with LDParent with PrimitiveNode {
+  def bls(fromPar:Array[VL],fromParD:Array[VL]) = {
+    (super.bls, thisBls(fromPar,fromParD)).zipped.map(_ + _)
+  }
+}
+
+case class LDRoot(children:List[LDChild], trans:ML, transD:ML, alpha:Array[VL], alphaD:Array[VL],
+                beta:Array[VL], betaD:Array[VL], model:Model) extends PrimitiveRoot with LDParent{
+  override def leafList = super.leafList.reverse
+  override def toList = super.toList.reverse
+}
+
+object LDLeaf extends LDTreeUtilTrait{
+  def inside(name:String,t:Double,column:Array[Base],m:Model,isTarget:Boolean):LDLeaf = {
+    val alpha = mkAlpha(column)
+    val alphaD = if(isTarget) alpha else Array.fill(alpha.length)(DenseVector.zeros[Double](4).toLogDouble)
+    val trans = mkTrans(t,m)
+    val transD = DenseMatrix.zeros[Double](4,4).toLogDouble
+    for(i <- 0 to 3) {transD(i,i) = trans(i,i)}
+    new LDLeaf(name,t,trans,transD,alpha,alphaD,null,null,m)
+  }
+
+  protected def mkAlpha(column:Array[Base]): Array[VL] = column.map{
+    case Base.N =>
+      DenseVector.ones[LogDouble](4)
+    case x =>
+      val tmp = DenseVector.zeros[Double](4)
+      tmp(x.toInt) = 1.0
+      tmp.toLogDouble
+  }
+}
+
+object LDNode extends LDTreeUtilTrait{
+  def inside(ch:List[LDChild],t:Double,m:Model) = {
+    val props = ch.map(_.insideProp)
+    val alpha = mkAlpha(props)
+    val alphaD = mkAlphaD(props, ch.map(_.insidePropD))
+    val trans = mkTrans(t,m)
+    val transD = DenseMatrix.zeros[Double](4,4).toLogDouble
+    for(i <- 0 to 3) {transD(i,i) = trans(i,i)}
+    new LDNode(ch,t,trans,transD,alpha,alphaD,null,null,m)
+  }
+}
+
+object LDRoot extends LDTreeUtilTrait{
+  def inside(ch:List[LDChild],m:Model) = {
+    val props = ch.map(_.insideProp)
+    val alpha = mkAlpha(props)
+    val alphaD = mkAlphaD(props, ch.map(_.insidePropD))
+    val trans = diag(DenseVector.ones[Double](4)).toLogDouble
+    val transD = DenseMatrix.zeros[Double](4,4).toLogDouble
+    for(i <- 0 to 3) {transD(i,i) = trans(i,i)}
+    new LDRoot(ch,trans,transD,alpha,alphaD,null,null,m)
+  }
+}
+
+trait LDTreeUtilTrait {
+  def mkAlpha(fromChildren:List[Array[VL]]):Array[VL] =
+    fromChildren.reduceLeft{(ns,xs) => (ns,xs).zipped.map(_ :* _)}
+  def mkAlphaD(fromChildren:List[Array[VL]],fromChildrenD:List[Array[VL]]):Array[VL] = {
+    val tmp:List[List[Array[VL]]] = mkConfound(fromChildren,fromChildrenD)
+    tmp.map(mkAlpha).reduce((n,x) => (n,x).zipped.map(_ + _))
+  }
+  def mkTrans(ti:Double,mi:Model):ML = {
+    val tmp: DenseMatrix[Double] = mi.u * diag(exp(mi.lambda * ti)).*(mi.ui)
+    tmp.toLogDouble
+  }
+  def mkConfound[T](base:List[T],confounder:List[T]) = {
+    @tailrec
+    def f(i:List[T], t:List[T], b:List[T], result:List[List[T]]):List[List[T]] = {
+      if(b.isEmpty)
+        result.reverse
+      else
+        f((t.head :: i.reverse).reverse, t.tail, b.tail, (t.tail.reverse ::: (b.head :: i.reverse)).reverse :: result)
+    }
+    f(Nil,base,confounder,Nil)
+  }
+}
+
+/*
 import java.io.FileReader
 import alignment.Base
 import breeze.linalg.{sum, DenseMatrix, DenseVector, diag}
---
+++ src/test/scala/eea/tree/EEATest.scala
@@ -0,0 +1,141 @@
+package eea.tree
+
+import alignment.Base._
+import alignment.Base
+import breeze.linalg.{diag, DenseMatrix, DenseVector}
+import breeze.numerics.log
+import fdur.Parameters
+import org.scalatest.FunSuite
+
+class EEATest extends FunSuite with LDTreeUtilTrait{
+
+  /*test("constructor"){
+    val tree = fdur.ModelTree.fromFile("src/test/resources/brown.trees")
+    assert(tree.names == List("Human","Chimpanzee","Gorilla"))
+    /*def f(t:LDTree):List[Double] = t match {
+      case LDLeaf(_,x) => List(x)
+      case LDNode(l,r,x) => f(l) ++ f(r) ++ List(x)
+      case LDRoot(l,r) => f(l) ++ f(r)
+    }*/
+    assert(tree.branches == List(0.2,0.1,0.5,0.3))
+  }*/
+
+  /*test("set_columns"){
+    val tree = fdur.ModelTree.fromFile("src/test/resources/brown.trees")
+    val column = List(C,A,G)
+    tree.setColumn(column)
+    assert(tree.column == column)
+  }*/
+
+  /*test("set_model"){
+    val tree = fdur.ModelTree.fromFile("src/test/resources/brown.trees")
+    tree.setModel(fdur.GTR())
+    def f(t:Tree):List[EvolutionModel] = t match {
+      case Leaf(_,_) => List(t.m)
+      case Node(l,r,_) => f(l) ++ f(r) ++ List(t.m)
+      case Root(l,r) => f(l) ++ f(r)
+    }
+    assert(f(tree).forall(_ == f(tree).head))
+  }*/
+
+  /*test("set_target"){
+    val tree = fdur.ModelTree.fromFile("src/test/resources/brown.trees")
+    //tree.setColumn(List(C,G,T))
+    //tree.setTarget("Chimpanzee")
+    /*def f(t:LDTree):List[LDTree] = t match {
+      case LDLeaf(_,_) => List(t)
+      case LDNode(ch,_) => f(l) ++ f(r)
+      case LDRoot(,r) => f(l) ++ f(r)
+    }*/
+
+    val tmp = (0 to 3) map {x => if(x == G.toInt) 1.0 else 0.0}
+    val l = LDTree.inout(tree,model,)
+    assert(l.head.insideD == DenseVector.zeros[Double](4))
+    assert(l(1).insideD == DenseVector(tmp.toArray))
+    assert(l(2).insideD == DenseVector.zeros[Double](4))
+  }*/
+
+  test("confound"){
+    val x = List(1,2,3)
+    val y = List(4,5,6)
+    val tmp = mkConfound(x,y)
+    val hoge = List(List(4,2,3),List(1,5,3),List(1,2,6))
+    assert(tmp == hoge)
+
+    val a = DenseVector(1,2,3)
+    val b = DenseVector(4,5,6)
+    val c = diag(a) * DenseMatrix.ones[Int](3,3) * diag(b)
+    assert(c == DenseMatrix((4,5,6),(8,10,12),(12,15,18)))
+}
+
+  test("inside && outside"){
+    val model = fdur.Model(Parameters(DenseVector(0.3,0.1,0.1,0.2,0.2,0.1),
+      DenseVector(1.0,2.0,3.0,4.0)))
+    val tree = fdur.ModelTree.fromFile("src/test/resources/eea/hoge3.nh")
+    val cols:List[Array[Base]] = List(Array(Base.C),Array(Base.G),Array(Base.T))
+    val target = "hg18"
+    val ldtree = LDTree.inout(tree,model,cols,target)
+    val fldtree = fdur.LDTree.inout(tree,model,cols)
+    //setTransProb(tree)
+    //println(DenseMatrix((1.0,2.0,3.0,4.0),(4.0,1.0,2.0,3.0),(3.0,4.0,1.0,2.0),(2.0,3.0,4.0,1.0)))
+    val tmp = (0 to 3) map {x => if(x == C.toInt) 1.0 else 0.0}
+    import fdur._
+    (ldtree.toList,fldtree.toList).zipped.foreach((e,f) => assert(e.alpha.head == f.alpha.head))
+    //(ldtree.toList,fldtree.toList).zipped.foreach((e,f) => assert(e.beta.head == f.beta.head))
+    /*ldtree.toList.foreach{x =>
+      println(x.trans.value)
+      println(x.transD.value)
+      println(x.alpha.head.value)
+      println(x.alphaD.head.value)
+      println(x.beta.head.value)
+      println(x.betaD.head.value)
+      println()
+    }*/
+    val ians = List(
+      DenseVector(0.0, 1.0, 0.0, 0.0),
+      DenseVector(0.0, 0.0, 0.0, 0.0),
+      DenseVector(0.0, 0.0, 0.0, 0.0),
+      DenseVector(0.0, 0.003145705593451748, 0.0, 0.0)
+    )
+    val oans = List(
+      DenseVector(0.0, 0.0, 0.0, 0.0),
+      DenseVector(0.0, 0.12204610197844742, 0.0, 0.0),
+      DenseVector(0.0, 0.09761985381121624, 0.0, 0.0),
+      DenseVector(0.0, 0.0, 0.0, 0.0)
+    )
+    (ldtree.toList,ians).zipped.foreach{(x,y) => assert(x.alphaD.head.value == y)}
+    (ldtree.toList,oans).zipped.foreach{(x,y) => assert(x.betaD.head.value == y)}
+  }
+  test("hugahuga"){
+    val model = fdur.Model(Parameters(DenseVector(0.3,0.1,0.1,0.2,0.2,0.1),
+      DenseVector(1.0,2.0,3.0,4.0)))
+    val tree = fdur.ModelTree.fromFile("src/test/resources/eea/hoge3.nh")
+    val cols = fdur.Maf.readMaf("src/test/resources/fdur/small.maf",512)
+    //val cols = Array[List[Array[Base]]](List(Array(Base.C),Array(Base.G),Array(Base.T)))
+    val results = cols.map(c => LDTree.bls(tree,model,c,"hg18"))
+    println(results.head.mkString(","))
+  }
+/*
+  test("outside"){
+    val tree = Tree.fromFile("src/test/resources/brown.trees")
+    tree.setColumn(List(C,G,T))
+    tree.setTarget("Human")
+    tree.setModel(gtr)
+    setTransProb(tree)
+    assert(tree.outside == DenseVector(10.0,20.0,30.0,40.0))
+    assert(tree.outsideD == DenseVector.zeros[Double](4))
+    assert(tree.left.outside == DenseVector(520.0,280.0,1040.0,600.0))
+    assert(tree.left.outsideD == DenseVector(0.0,0.0,0.0,0.0))
+    assert(tree.right.outsideD == DenseVector(0.0,0.0,520.0,0.0))
+  }
+*/
+  /*def setTransProb(t:Tree):Unit = {
+    t match {
+      case Leaf(_, _) => Unit
+      case Node(l, r, _) => setTransProb(r);setTransProb(l)
+      case Root(l, r) => setTransProb(l);setTransProb(r)
+    }
+    t.transProb = DenseMatrix((1.0,2.0,3.0,4.0),(4.0,1.0,2.0,3.0),(3.0,4.0,1.0,2.0),(2.0,3.0,4.0,1.0))
+    t.transProbD = DenseMatrix((1.0,0.0,0.0,0.0),(0.0,1.0,0.0,0.0),(0.0,0.0,1.0,0.0),(0.0,0.0,0.0,1.0))
+  }*/
+}
